946fa115352a5d78fb00344274ac8332e84cfcbd
rock <rock0715@outlook.com>
2012-12-24 14:51:04 +0800

    logresult

---
+++ src/main/java/mx/spring/test/data/entity/LogResultEntity.java
@@ -0,0 +1,152 @@
+package mx.spring.test.data.entity;
+
+import java.util.Date;
+
+public class LogResultEntity {
+
+	private int id;
+	private String fileName;
+	private String mtype;
+	private String beginDateTime;
+	private String endDateTime;
+	private String key1;
+	private String key2;
+	private String key3;
+	private String key4;
+	private String key5;
+	private String key6;
+	private String key7;
+	private String key8;
+	private String key9;
+	private String key10;
+	private Date createDt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getMtype() {
+		return mtype;
+	}
+
+	public void setMtype(String mtype) {
+		this.mtype = mtype;
+	}
+
+	public String getBeginDateTime() {
+		return beginDateTime;
+	}
+
+	public void setBeginDateTime(String beginDateTime) {
+		this.beginDateTime = beginDateTime;
+	}
+
+	public String getEndDateTime() {
+		return endDateTime;
+	}
+
+	public void setEndDateTime(String endDateTime) {
+		this.endDateTime = endDateTime;
+	}
+
+	public String getKey1() {
+		return key1;
+	}
+
+	public void setKey1(String key1) {
+		this.key1 = key1;
+	}
+
+	public String getKey2() {
+		return key2;
+	}
+
+	public void setKey2(String key2) {
+		this.key2 = key2;
+	}
+
+	public String getKey3() {
+		return key3;
+	}
+
+	public void setKey3(String key3) {
+		this.key3 = key3;
+	}
+
+	public String getKey4() {
+		return key4;
+	}
+
+	public void setKey4(String key4) {
+		this.key4 = key4;
+	}
+
+	public String getKey5() {
+		return key5;
+	}
+
+	public void setKey5(String key5) {
+		this.key5 = key5;
+	}
+
+	public String getKey6() {
+		return key6;
+	}
+
+	public void setKey6(String key6) {
+		this.key6 = key6;
+	}
+
+	public String getKey7() {
+		return key7;
+	}
+
+	public void setKey7(String key7) {
+		this.key7 = key7;
+	}
+
+	public String getKey8() {
+		return key8;
+	}
+
+	public void setKey8(String key8) {
+		this.key8 = key8;
+	}
+
+	public String getKey9() {
+		return key9;
+	}
+
+	public void setKey9(String key9) {
+		this.key9 = key9;
+	}
+
+	public String getKey10() {
+		return key10;
+	}
+
+	public void setKey10(String key10) {
+		this.key10 = key10;
+	}
+
+	public Date getCreateDt() {
+		return createDt;
+	}
+
+	public void setCreateDt(Date createDt) {
+		this.createDt = createDt;
+	}
+
+}
